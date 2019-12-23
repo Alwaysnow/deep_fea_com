b9f2606b8d24f760a2dd785bfd2c6265aedf231a
Alwaysnow <2512738105@qq.com>
2019-12-23 19:54:52 +0800

    update model.py

--- model.py
+++ model.py
@@ -1,1 +1,16 @@
 import tensorflow as tf 
+import numpy as np 
+
+def encoder_non(input):
+    with tf.variable_scope("fea_non"):
+        output_1 = tf.layers.conv2d(input, 64, [1,1], strides=[1,1], padding='same')
+        output_1 = tf.contrib.layers.gdn(output_1)
+        output_2 = tf.layers.conv2d(output_1, 64, [1,1], strides=[1,1], padding='same')
+        return output_2
+
+def decoder_non(input):
+    with tf.variable_scope("fea_non"):
+        output_1 = tf.layers.conv2d(input, 128, [1,1], strides=[1,1], padding='same')
+        output_1 = tf.contrib.layers.gdn(output_1, inverse=True)
+        output_2 = tf.layers.conv2d(output_1, 128, [1,1], strides=[1,1], padding='same')
+        return output_2
